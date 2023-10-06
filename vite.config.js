2a7b5eed96f77c12e1e98b5730c1fbc9534fa29c
neco8 <shogibeshow@gmail.com>
2023-10-06 10:59:31 +0900

    add: PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
 import sassPlugin from "vite-plugin-sass";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [elmPlugin(), sassPlugin()],
+  plugins: [
+    elmPlugin(),
+    sassPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   build: {
     outDir: "_site",
   },
